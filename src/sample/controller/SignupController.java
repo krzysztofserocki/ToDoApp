f25cfacadc77d64063228a59afe558010bd5f7a6
krzysztofserocki <leicer141@gmail.com>
2019-03-21 14:30:53 +0100

    Added addItem and signup views and controllers for them

---
+++ src/sample/controller/SignupController.java
@@ -0,0 +1,44 @@
+package sample.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class SignupController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField signupFirstName;
+
+    @FXML
+    private JFXTextField signupLastName;
+
+    @FXML
+    private JFXTextField signupUsername;
+
+    @FXML
+    private JFXPasswordField signupPassword;
+
+    @FXML
+    private JFXCheckBox signupCheckBoxMale;
+
+    @FXML
+    private JFXCheckBox signupCheckBoxFemale;
+
+    @FXML
+    private JFXButton signupButton;
+
+    @FXML
+    void initialize() {
+
+    }
+}
