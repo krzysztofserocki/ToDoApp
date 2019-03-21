ee8e36a892694a2e1f9b96eaa2539b3f5417b712
krzysztofserocki <leicer141@gmail.com>
2019-03-21 15:00:30 +0100

    Created database and tables. Connected app to db

---
+++ src/sample/Database/DatabaseHandler.java
@@ -0,0 +1,21 @@
+package sample.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler extends  Configs{
+    Connection dbConnection;
+
+    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        String connectionString = "jdbc:mysql://" + dbHost + ":" + dbPort
+                                  + "/" + dbName + "?useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+
+        return dbConnection;
+    }
+
+}
