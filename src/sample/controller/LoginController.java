df23968a3615fa1168f239f6f3e90821ff7d6264
krzysztofserocki <leicer141@gmail.com>
2019-03-21 15:12:45 +0100

    Added connection beetwen Login and Signup screens

--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -3,9 +3,15 @@ package sample.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -29,9 +35,39 @@ public class LoginController {
 
     @FXML
     void initialize() {
-        loginButton.setOnAction(event -> {
-            System.out.println("Login button is clicked!");
+        String logUsr = loginUsername.getText().trim();
+        String logPass = loginPassword.getText().trim();
+
+        loginSignupButton.setOnAction(event -> {
+            //Take users to signup screen
+            loginSignupButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/view/signup.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
+
+        loginButton.setOnAction(event -> {
+            if (!logPass.equals("") || !logPass.equals("")) {
+                loginUser(logUsr, logPass);
+            } else {
+                System.out.println("Error login in user");
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        //Check in the database if the user exists, if true
+        //We take them to AddItem screen
+
     }
 }
 
--- src/sample/controller/SignupController.java
+++ src/sample/controller/SignupController.java
@@ -4,9 +4,15 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class SignupController {
 
